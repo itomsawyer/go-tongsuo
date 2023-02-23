611590ffe23e4a18afc533e364871f1a07d1258c
itomsawyer <tomsawyer126@gmail.com>
2023-02-23 11:51:29 +0800

    fix ld library not found

--- build.go
+++ build.go
@@ -15,10 +15,10 @@
 package tongsuo
 
 // #cgo linux CFLAGS: -Wno-deprecated-declarations -I/usr/local/opt/tongsuo/include
-// #cgo linux LDFLAGS: -static -L/usr/local/opt/tongsuo/lib64  -lssl -lcrypto
+// #cgo linux LDFLAGS: -L/usr/local/opt/tongsuo/lib64  -lssl -lcrypto
 // #cgo darwin CFLAGS: -I/opt/tongsuo/include -Wno-deprecated-declarations
-// #cgo darwin LDFLAGS: -static  -L/opt/tongsuo/lib -lssl -lcrypto
+// #cgo darwin LDFLAGS: -L/opt/tongsuo/lib -lssl -lcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
-// #cgo windows pkg-config: --static libssl libcrypto
+// #cgo windows pkg-config: libssl libcrypto
 // #include "shim.h"
 import "C"
--- crypto/sm3/build.go
+++ crypto/sm3/build.go
@@ -5,6 +5,6 @@ package sm3
 // #cgo darwin CFLAGS: -I/opt/tongsuo/include -Wno-deprecated-declarations
 // #cgo darwin LDFLAGS: -L/opt/tongsuo/lib -lssl -lcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
-// #cgo windows pkg-config: --static libssl libcrypto
+// #cgo windows pkg-config: libssl libcrypto
 // #include "../../shim.h"
 import "C"
